9b6cb25b9faf828d68110320be3b8df096c0e80f
Tmreign <tomwanyoike9@gmail.com>
2019-08-07 17:14:08 +0300

    deploy

--- src/test/java/DatabaseRule.java
+++ src/test/java/DatabaseRule.java
@@ -6,7 +6,7 @@ public class DatabaseRule extends ExternalResource {
 
     @Override
     protected void before() {
-        DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_test", "moringaschool", "1234");
+        DB.sql2o = new Sql2o("jdbc:postgresql://ec2-174-129-231-25.compute-1.amazonaws.com:5432/d8vi119m4qfk34", "akdscbrcazssbr", "2c1c9fca8d1bdf7e85d3a6ac1a59990cdec49be1b0ca6c638bd32b6f1d53363f");
     }
 
     @Override
